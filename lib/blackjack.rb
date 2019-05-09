6203f2cab7e6cf657616ed9180ac8c205dbfd0b6
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-09 04:55:06 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,7 +25,9 @@ end
 def initial_round
   card1 = deal_card
   card2 = deal_card
-  display_card_total(card1 + card2)
+  total = card1 + card2
+  display_card_total(total)
+  total
 end
 
 def hit?
